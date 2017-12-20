145aae2fff0358aeec908b809b966842ec412982
Brian <brian@example.com>
2017-12-19 23:34:41 -0600

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -25,13 +25,13 @@ app.post('/todos',  (req, res)  =>  {
 
 //GET /todos/123afafasdf
 
-// app.get('/todos',  (req, res)  =>  {
-//   Todo.find().then((todos)  =>  {
-//     res.send({todos});
-//   }, (e)  =>  {
-//     res.status(400).send(e);
-//   })
-// });
+app.get('/todos',  (req, res)  =>  {
+  Todo.find().then((todos)  =>  {
+    res.send({todos});
+  }, (e)  =>  {
+    res.status(400).send(e);
+  })
+});
 
 ///
 
