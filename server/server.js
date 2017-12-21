283b11e4d2b2205937ab7a2dbb9ee5596673c2fc
Brian <brian@example.com>
2017-12-20 21:13:12 -0600

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -58,6 +58,28 @@ app.get('/todos/:id',  (req, res)  =>  {
   })
 });
 
+app.delete('/todos/:id',  (req, res)  =>  {
+  var id =  req.params.id;
+  if(!ObjectID.isValid(id)) {
+    return  res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) =>  {
+    if (!todo) {
+      return  res.status(404).send();
+    }
+
+    res.send(todo);
+  }).catch((e)  =>  {
+    res.status(400).send();
+  })
+
+
+
+  //validate
+});
+
+
 app.listen(port/*3000*/, ()  =>  {
   console.log(`Started up at port ${port}`);
 });
